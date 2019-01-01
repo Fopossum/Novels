eb791d00fe4ca59f95ec5a143674ceb454036317
Fred Passerin <fopossum@gmail.com>
2019-01-01 23:22:58 +0100

    Reprise partielle chapitres 5 et 6, modifications de la génération du pdf

--- Le caveau/do_xhtml.sh
+++ Le caveau/do_xhtml.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 
-#mk4ht xhlatex $1 "xhtml, charset=utf-8" " -cunihtf -utf8" -d "./xhtml/"
-mk4ht xhlatex $1 
+mk4ht xhlatex $1 "xhtml, charset=utf-8" " -cunihtf -utf8" -d "./xhtml/"
+#mk4ht xhlatex $1 
